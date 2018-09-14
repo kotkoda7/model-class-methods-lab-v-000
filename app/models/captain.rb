b065e1bb55c2481b022509ed267369b42a556ef4
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-14 02:03:15 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,11 @@ class Captain < ActiveRecord::Base
   has_many :boats
   
   def self.catamaran_operators
-  #returns all captains of catamarans
+    #returns all captains of catamarans
   
+  end
+  
+  def self.sailors
+    #
   end
 end
